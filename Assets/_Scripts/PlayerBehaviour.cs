38cf9edb1d2aedc317b34a240656579732aeaf4b
Hakan <hknaslanoglu@gmail.com>
2020-11-23 14:34:40 -0500

    added PlayerBehaviourScript

---
+++ Assets/_Scripts/PlayerBehaviour.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    private Rigidbody2D rigidbody2D;
+    private SpriteRenderer spriteRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetAxis("Horizontal") > 0.1f)
+        {
+            // move right
+            spriteRenderer.flipX = false;
+        }
+        if(Input.GetAxis("Horizontal") < -0.1f)
+        {
+            //move left
+            spriteRenderer.flipX = true;
+        }
+    }
+}
